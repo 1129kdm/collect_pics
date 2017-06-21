e3885ed2723196641e277f3584a56b467ed9e20e
kdm <1129nikuchiku@gmail.com>
2017-06-21 23:03:36 +0900

    - add collect media_url in tweets - add haraeki package

--- src/github.com/1129kdm/main.go
+++ src/github.com/1129kdm/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/1129kdm/haraeki"
 	"github.com/1129kdm/twitter"
+	"net/url"
 )
 
 func main() {
 	twitterApi := twitter.AuthTwitterApi()
-	fmt.Println(twitterApi)
+	v := url.Values{}
+	v.Set("screen_name", "nakano_aimi")
+	v.Set("count", "1")
 
-	text := "test ichi chan"
-	tweet, err := twitterApi.PostTweet(text, nil)
+	tweets, err := twitterApi.GetUserTimeline(v)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println(tweet.Text)
+	for _, tweet := range tweets {
+		if tweet.Entities.Media != nil {
+			imgUrl := tweet.Entities.Media[0].Media_url
+			fmt.Println("tweet: ", imgUrl)
+		}
+	}
+
+	fmt.Println(haraeki.MemberTwitterNames())
+	twitterApi.Close()
 }
